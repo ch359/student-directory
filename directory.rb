047ff1385fc8eb5b3bf78dcb05ff887bdb289302
Colin Hulme <17675453+ch359@users.noreply.github.com>
2018-12-13 11:28:14 +0000

    Ex 8

--- directory.rb
+++ directory.rb
@@ -1,10 +1,12 @@
+COHORTS =  ["january", "february", "march", "april", "june", "july", "august", "september",
+            "october", "november", "december"]
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  cohorts = ["january", "february", "march", "april", "june", "july", "august", "september",
-            "october", "november", "december"]
+
   # get the first name
   name = gets.chomp
 
@@ -16,7 +18,7 @@ def input_students
     puts "Please enter a cohort"
     cohort = gets.chomp
 
-    if !cohorts.include?(cohort.downcase)
+    if !COHORTS.include?(cohort.downcase)
       cohort = "november"
     end
 
@@ -30,20 +32,19 @@ def input_students
   students
 end
 
+def print_students_by_cohort(students)
+  get_cohorts(students).each do |cohort, names|
+    print "\n#{cohort} cohort students: "
+    names.each { |name| print "#{name}, "}
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_students_by_cohort(students)
-  student_cohorts = get_cohorts(students)
-  student_cohorts.each do
-    
-  end
-
-def print(students)
-
-
+def print_students(students)
   students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort). Hobby: #{student[:hobby]}".center(80)
   end
@@ -54,16 +55,20 @@ def print_footer(students)
 end
 
 def get_cohorts(student_array)
-  student_cohorts = []
+  student_cohorts = {}
   student_array.each do |student|
-    if !student_cohorts.include?(student[:cohort])
-      student_cohorts.push()
+    if student_cohorts.has_key?(student[:cohort])
+      student_cohorts[student[:cohort]].push(student[:name])
+    else
+      student_cohorts[student[:cohort]] = [student[:name]]
     end
-
+  end
+  student_cohorts
 end
 
 students = input_students
 #nothing happens until we call the methods
 print_header
-print(students)
+print_students(students)
 print_footer(students)
+print_students_by_cohort(students)
