5132b2a619dc179c9320cddcb465af4d935b1ff9
Colin Hulme <17675453+ch359@users.noreply.github.com>
2018-12-17 16:39:06 +0000

    14.5 Allow user filename input

--- directory.rb
+++ directory.rb
@@ -64,8 +64,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list (default: students.csv)"
+  puts "4. Load the list (default: students.csv)"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -75,8 +75,8 @@ def show_students
   print_footer
 end
 
-def save_students
-  file = File.open("students.csv", "w")
+def save_students(filename = "students.csv")
+  file = File.open(filename, "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -98,8 +98,7 @@ def add_name_and_cohort(name, cohort)
   @students << {name: name, cohort: cohort.to_sym}
 end
 
-def try_load_students
-  filename = ARGV.first
+def try_load_students(filename = ARGV.first)
   return if filename.nil?
   if File.exist?(filename)
     load_students(filename)
@@ -108,6 +107,11 @@ def try_load_students
     puts "Sorry, #{filename} doesn't exist."
     exit
   end
+end
+
+def ask_filename
+  puts "Please enter the (relative) filename you wish to use, followed by enter"
+  STDIN.gets.chomp
 end
 
 def menu_choices(selection)
@@ -120,10 +124,10 @@ def menu_choices(selection)
       show_students
   when "3"
       puts "Selected Option 3"
-      save_students
+      save_students(ask_filename)
   when "4"
       puts "Selected Option 4"
-      try_load_students
+      try_load_students(ask_filename)
   when "9"
       puts "Quitting the program"
       exit # this will cause the program to terminate
